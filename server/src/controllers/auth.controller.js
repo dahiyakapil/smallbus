a2e05e3e2d6c457cad1ff887e1aa2c947d494a19
KAPIL DAHIYA <kapildahiya309@gmail.com>
2025-08-21 16:39:12 +0530

    login and get profile

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,36 +1,87 @@
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
 import { validateSignupData } from "../utils/validation.js";
+import validator from "validator";
 
 export const signup = async (req, res) => {
-  try {
-   
-    validateSignupData(req);
+    try {
 
-    const { name, email, password } = req.body;
+        validateSignupData(req);
+
+        const { name, email, password } = req.body;
 
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(409).json({ message: "Email already exists" });
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(409).json({ message: "Email already exists" });
+        }
+
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const user = new User({ name, email, password: passwordHash });
+        const savedUser = await user.save();
+
+
+        const token = await savedUser.getJWT();
+
+        return res.status(201).json({
+            message: "User created successfully",
+            token,
+        });
+    } catch (error) {
+        return res
+            .status(400)
+            .json({ error: "Error in creating the user: " + error.message });
     }
+};
 
 
-    const passwordHash = await bcrypt.hash(password, 10);
+export const login = async (req, res) => {
+    try {
 
-    const user = new User({ name, email, password: passwordHash });
-    const savedUser = await user.save();
+        const { email, password } = req.body;
 
-    
-    const token = await savedUser.getJWT();
 
-    return res.status(201).json({
-      message: "User created successfully",
-      token,
-    });
-  } catch (error) {
-    return res
-      .status(400)
-      .json({ error: "Error in creating the user: " + error.message });
-  }
+
+        if (!email || !password || !validator.isEmail(email)) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const isPasswordValid = await user.validatePassword(password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const token = await user.getJWT();
+
+        return res.json({
+            message: "Login successful",
+            token,
+        });
+    } catch (error) {
+        console.error("Login error:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+
+export const getProfile = async (req, res) => {
+    try {
+        const user = req.user;
+        console.log(user)
+
+        return res.json({
+            name: user.name,
+            email: user.email
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
 };
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { getProfile, login, signup } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 
 
 const authRouter = express.Router();
 
 
 authRouter.post("/signup", signup);
+authRouter.post("/login", login);
+authRouter.get("/profile", authMiddleware, getProfile);
 
 export default authRouter;
